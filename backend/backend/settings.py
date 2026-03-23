22bd0fe52c17343dbfed3301d2736a680e1b1fa6
AiyanaCookey <167078825+AiyanaCookey2024@users.noreply.github.com>
2026-03-23 22:25:08 +0000

    checking admin functionalities work

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -132,9 +132,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = []
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
